aea0d49205f55dd840d4644405a7953a1c795ee1
George Bastos <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2025-02-26 20:41:16 -0300

    Update BarraDePesquisa.jsx

--- src/components/BarraDePesquisa.jsx
+++ src/components/BarraDePesquisa.jsx
@@ -36,25 +36,6 @@ export const BarraDePesquisa = ({ searchKey }) => {
           value={buscar}
           onChange={(e) => setBuscar(e.target.value)}
         />
-        <button
-          className="btn btn-sm md:btn-md bg-green-500 border-green-300 hover:bg-green-200 text-white"
-          onClick={() => setSearchParams({ search: buscar })}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4 md:h-6 md:w-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
       </div>
     </div>
   );
